621ec7486f1db5b5c156f72a08eca3dddd8a2332
SUK HWA HAM <hamsukhwa@gmail.com>
2019-03-28 23:35:42 -0400

    More Controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ export const postJoin = (req, res) => {
         res.render("join", { pageTitle: "Join" });
     } else {
         // To Do: Register User
-        // To Do: Log user in
+        // To Do: Log user in``
         res.redirect(routes.home);
     }
 };
@@ -24,8 +24,11 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-    res.render("logout", { pageTitle: "Log Out" });
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) =>
     res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
