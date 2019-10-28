496d9c0d6fe41110a6c80e952c68697bdea6d610
SUK HWA HAM <hamsukhwa@gmail.com>
2019-10-27 21:06:43 -0400

    Fix error github login

--- src/passport.js
+++ src/passport.js
@@ -16,7 +16,7 @@ passport.use(
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
       callbackURL: process.env.PRODUCTION
-        ? `https://stark-citadel-98760.herokuapp.com${routes.githubCallback}`
+        ? `https://salty-atoll-60969.herokuapp.com${routes.githubCallback}`
         : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
